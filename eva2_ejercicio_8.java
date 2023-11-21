9af3bc3fbfbb713fc2f9b22c1339aa05ff2b108e
Emily365sanchez <144498285+Emily365sanchez@users.noreply.github.com>
2023-11-21 17:47:26 -0600

    Add files via upload

---
+++ eva2_ejercicio_8.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author invitado
+ */
+public class eva2_ejercicio_8 {
+    Scanner scanner = new Scanner(System.in);
+        char letra;
+        
+        System.out.println("digite una letra: ");
+        char = scanner.next().charAt(0);
+        
+                
+                
+                
+    
+        switch (letra) static {
+            case 'a':
+            case'e':
+            case'i':
+            case'o':
+            case'u':
+                System.out.println("es vocal");
+               break;
+                
+            case'b':
+                case'c':
+                case'd':
+                case'f':
+                case'g':
+                case'h':
+                case'j':
+                case'k':
+                case'l':
+                case'm':
+                case'n':
+                case'ñ':
+                case'q':
+                case'r':
+                case's':
+                case't':
+                case'v':
+                case'w':
+                case'y':
+                case'z':
+                    System.out.println("es consonante");
+                    break;
+             
+                
+                
+            
+        }
+}
